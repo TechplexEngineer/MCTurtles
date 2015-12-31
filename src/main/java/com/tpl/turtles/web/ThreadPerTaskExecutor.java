887145f350d393fe7cabe7f1e4f3dd3762204093
Blake Bourque <Techplex.Engineer@gmail.com>
2015-12-31 16:03:53 -0500

    Use sync bukkit scheduling for webserver threads to make calling into the bukkit api safe.

--- src/main/java/com/tpl/turtles/web/ThreadPerTaskExecutor.java
+++ src/main/java/com/tpl/turtles/web/ThreadPerTaskExecutor.java
@@ -5,7 +5,10 @@
  */
 package com.tpl.turtles.web;
 
+import com.tpl.turtles.Main;
 import java.util.concurrent.Executor;
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitScheduler;
 
 /**
  * https://docs.oracle.com/javase/8/docs/api/index.html?java/util/concurrent/Executor.html
@@ -15,6 +18,7 @@ import java.util.concurrent.Executor;
 public class ThreadPerTaskExecutor implements Executor {
 	@Override
 	public void execute(Runnable r) {
-		new Thread(r).start();
+		BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+		scheduler.runTaskLater(Main.inst, r, 0);
 	}
 }
